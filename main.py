0b37bce2962fa83c86639004c29409c7804be09b
ZhangFan <18702229693@163.com>
2018-11-01 04:45:14 +0800

    11.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aip import AipSpeech
 from tkinter import *
+from PIL import Image, ImageTk
 import time
 import pyaudio
 from getWeather import *
@@ -29,8 +30,8 @@ def callback():
         infoString.set(WeatherInfo)
         result  = client.synthesis(WeatherInfo, 'zh', 1, {'vol': 3})
     else:
-        infoString.set('识别失败，请清晰地再说一次。')
-        result  = client.synthesis('识别失败，请清晰地再说一次。', 'zh', 1, {'vol': 3})
+        infoString.set('识别失败\n请清晰地再说一次。')
+        result  = client.synthesis('识别失败,请清晰地再说一次。', 'zh', 1, {'vol': 3})
     if not isinstance(result, dict):
         aud=open(resPath, 'wb')
         aud.write(result)
@@ -38,14 +39,14 @@ def callback():
         AudioPlay(resPath)
 
 
-APP_ID = '喵'
-API_KEY = '喵'
-SECRET_KEY = '喵'
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
 
 client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 root=Tk()
 root.title("What Weather Today")
-root.geometry('400x300')
+root.geometry('400x600')
 mainString=StringVar()
 mainString.set('按下按钮，说出城市名')
 btnString=StringVar()
@@ -53,14 +54,25 @@ btnString.set('按下聆听')
 infoString=StringVar()
 infoString.set(' ')
 
-mainLabel = Label(root, textvariable=mainString, font=("Arial", 12), width=20, height=5)
-mainLabel.pack(side=TOP)
+image = Image.open("./images/Artboard.png") 
+photo = ImageTk.PhotoImage(image)
+bkLabel=Label(root,image=photo)
+bkLabel.place(x=0,y=0,relwidth=1, relheight=1) 
+
+
+
+photo1 = ImageTk.PhotoImage(image)
+mainLabel = Label(root, textvariable=mainString,compound='center',image=photo1,font=("Arial", 15),fg='white')
+mainLabel.place(x=125,y=20,width=150,height=50)
+photo2 = ImageTk.PhotoImage(file='./images/2.gif')
+infoLabel = Label(root, textvariable=infoString,compound='center',image=photo2, font=("Arial", 15),fg='black')
+infoLabel.place(x=200,y=300,width=195,height=295)
 
 mainButton = Button(root,textvariable=btnString,command=callback)
-mainButton.pack()
+mainButton.place(x=155,y=205)
 
-infoLabel = Label(root, textvariable=infoString, font=("Arial", 12), width=30, height=30)
-infoLabel.pack()
+
+
 root.mainloop()
 
 
